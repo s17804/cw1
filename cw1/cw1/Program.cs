c4cf3ae01a6bd48a2670a61884966b8210530d34
margrim <margrim88@gmail.com>
2020-03-01 12:34:01 +0100

    Finished project

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            var webPage = args[0];
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(webPage);
+            var content = response.Content;
+            var result = await content.ReadAsStringAsync();
+
+            var regex = new Regex(pattern);
+            foreach (var word in result.Split(' '))
+            {
+                var match = regex.Match(word);
+                if (match.Success)
+                {
+                    Console.WriteLine(word);
+                }
+            }
         }
     }
 }
